3ec12e2c545ee78d21b1509e3132b99c73a62589
AurelienSurier <aurelien.surier@gmail.com>
2020-06-04 15:10:02 +0200

    Fixed hardcodes in postinstall

--- postinstall.js
+++ postinstall.js
@@ -28,45 +28,42 @@ console.log(options)
 if (options.db === "sql") {
     
 } else if (options.db === "nosql") {
-    mongoose.connect('mongodb://localhost:27017/files', { useNewUrlParser: true, useUnifiedTopology: true });
-    var db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-    var FileSchema = new mongoose.Schema({ name: String, creation: Date, md5sum: String, sha1sum: String, path: String, size: Number })
-    var File = mongoose.model('File', FileSchema);
-    var AuthApiKeySchema = new mongoose.Schema({ apikey: String })
-    var AuthApiKey = mongoose.model('AuthApiKey', AuthApiKeySchema)
-
-    for (const method of defs.authorizations) {   
-        switch (method.type) {
-            case 'token':
-                for (const token of method.tokens) {
-                    console.log("[Store] Adding token " + token)
-                    insertToken(token).then(() => console.log("... token inserted")).catch((e) => { console.log(e) })
-                }
-                
-                break;
-        }
-    }
+    populateNoSql();
 
 } else {
     console.log("- no store installed");
 }
 
-process.exit(0)
 
+async function populateNoSql(){
+        mongoose.connect(`mongodb://${defs.store.host}:${defs.store.port}/${defs.store.name}`, { useNewUrlParser: true, useUnifiedTopology: true });
+        var db = mongoose.connection;
+        db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-async function insertToken(token){
-    return new Promise((resolve, reject) => {
-        let o = new AuthApiKey();
-        o.apikey = token
-        o.save(function (err) {
-            if (err) {
-                console.log(err)
-                reject(err)
-            } else {
-                resolve()
+        var AuthApiKeySchema = new mongoose.Schema({ apikey: String })
+        var AuthApiKey = mongoose.model('AuthApiKey', AuthApiKeySchema)
+
+        for (const method of defs.authorizations) {
+            switch (method.type) {
+                case 'token':
+                    for (const token of method.tokens) {
+                        console.log("[Store] Adding token " + token)
+                        try {
+                            await insertToken(token, AuthApiKey)
+                        } catch (e) {
+                            console.log(e)
+                        }
+                    }
+
+                    break;
             }
-        });
-    });
+        }
+
+        process.exit(0)
+}
+
+async function insertToken(token, AuthApiKey){
+    let o = new AuthApiKey()
+    o.apikey = token
+    await o.save()
 }
